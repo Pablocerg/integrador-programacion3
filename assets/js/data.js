8a81f651552286804d0e5cb425ab45d459312a87
pablocerg <pablocerg@gmail.com>
2025-09-25 20:13:45 -0300

    [1.0.0] Refactor: nombre imagenes

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,11 +1,11 @@
 // assets/js/data.js
     export const initialProductos = [
-    { id: "101", nombre: "Pizza Margarita", descripcion: "Salsa, mozzarella, albahaca", categoria: "clasicas", precio: 20000, stock: 15, img: "assets/img/licensed-image.jpeg" },
-    { id: "102", nombre: "Pizza Pepperoni", descripcion: "Pepperoni, mozzarella", categoria: "especiales", precio: 25000, stock: 10, img: "assets/img/licensed-image1.jpeg" },
-    { id: "103", nombre: "Pizza Vegetariana", descripcion: "Vegetales mixtos", categoria: "vegetarianas", precio: 15800, stock: 8, img: "assets/img/licensed-image2.jpeg" },
-    { id: "104", nombre: "Calzone", descripcion: "Jamón, ricotta, mozzarella", categoria: "calzones", precio: 12000, stock: 7, img: "assets/img/licensed-image3.jpeg" },
-    { id: "105", nombre: "Pizza 4 Quesos", descripcion: "Mozzarella, provolone, parmesano, azul", categoria: "especiales", precio: 22000, stock: 9, img: "assets/img/licensed-image4.jpeg" },
-    { id: "106", nombre: "Pizza Hawaiana", descripcion: "Jamón y piña", categoria: "clasicas", precio: 19500, stock: 6, img: "assets/img/licensed-image5.jpeg" },
-    { id: "107", nombre: "Pizza Champiñones", descripcion: "Champiñones y orégano", categoria: "vegetarianas", precio: 17000, stock: 12, img: "assets/img/licensed-image6.jpeg" },
-    { id: "108", nombre: "Pizza Especial Casa", descripcion: "Ingredientes secretos", categoria: "especiales", precio: 28000, stock: 5, img: "assets/img/licensed-image7.jpeg" }
+    { id: "101", nombre: "Pizza Margarita", descripcion: "Salsa, mozzarella, albahaca", categoria: "clasicas", precio: 20000, stock: 15, img: "assets/img/margarita.jpeg" },
+    { id: "102", nombre: "Pizza Pepperoni", descripcion: "Pepperoni, mozzarella", categoria: "especiales", precio: 25000, stock: 10, img: "assets/img/pepperoni.jpeg" },
+    { id: "103", nombre: "Pizza Vegetariana", descripcion: "Vegetales mixtos", categoria: "vegetarianas", precio: 15800, stock: 8, img: "assets/img/vegetariana.jpeg" },
+    { id: "104", nombre: "Calzone", descripcion: "Jamón, ricotta, mozzarella", categoria: "calzones", precio: 12000, stock: 7, img: "assets/img/calzone.jpeg" },
+    { id: "105", nombre: "Pizza 4 Quesos", descripcion: "Mozzarella, provolone, parmesano, azul", categoria: "especiales", precio: 22000, stock: 9, img: "assets/img/cuatro-quesos.jpeg" },
+    { id: "106", nombre: "Pizza Hawaiana", descripcion: "Jamón y piña", categoria: "clasicas", precio: 19500, stock: 6, img: "assets/img/hawaiana.jpeg" },
+    { id: "107", nombre: "Pizza Champiñones", descripcion: "Champiñones y orégano", categoria: "vegetarianas", precio: 17000, stock: 12, img: "assets/img/champinones.jpeg" },
+    { id: "108", nombre: "Pizza Especial Casa", descripcion: "Ingredientes secretos", categoria: "especiales", precio: 28000, stock: 5, img: "assets/img/especial.jpeg" }
     ];
